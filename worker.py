98a1d7d21195f3bca9d4e14fd2c3a82b8e912971
CaniculaDUB <114599649+CaniculaDUB@users.noreply.github.com>
2022-12-20 17:17:32 +0300

    Update worker.py

--- worker.py
+++ worker.py
@@ -37,7 +37,7 @@ def addWorker(token, num):
     with open('payload.json') as p:
       tdata=json.load(p)
     tdata['name']='slave'+str(num)
-    #data=json.dumps(tdata)
+    data=json.dumps(tdata)
     #url='https://www.googleapis.com/compute/v1/projects/try2-371019/zones/europe-west1-b/instances'
     #headers={"Authorization": "Bearer "+token}
     #resp=requests.post(url,headers=headers, data=data)
